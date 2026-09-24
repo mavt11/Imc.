96c9f30f6dc9e1b5694c2f4812b3a141610ddac5
miguel angel vasconcelos tec <angelvascocelos@gmail.com>
2026-09-24 13:43:30 -0600

    se cambio el diseño de la iu para usar el grid layout

--- Imc.Maui/Views/MainPage.xaml.cs
+++ Imc.Maui/Views/MainPage.xaml.cs
@@ -4,29 +4,32 @@ namespace Imc.Maui.Views;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
+    public MainPage()
+    {
+        InitializeComponent();
+        SitucionNutricionalLabel.Text = string.Empty;
         ImcLabel.Text = string.Empty;
-        SituacionNutricionalLabel.Text = string.Empty;
-	}
+    }
 
     private void OnCalcularButtonClicked(object sender, EventArgs e)
     {
         decimal peso;
         decimal estatura;
-        peso = decimal.Parse(PesoEntry.Text);
+        peso = decimal.Parse(PesoENtry.Text);
         estatura = decimal.Parse(EstaturaEntry.Text);
         decimal imc = CalculadoraImc.IndiceDeMasaCorporal(peso, estatura);
         ImcLabel.Text = imc.ToString("F4");
-        SituacionNutricionalLabel.Text = CalculadoraImc.SituacionNutricional(imc); 
+        SitucionNutricionalLabel.Text = CalculadoraImc.SituacionNutricional(imc);
+
     }
 
     private void OnLimpiarButtonClicked(object sender, EventArgs e)
     {
-        PesoEntry.Text = string.Empty;
+        PesoENtry.Text = string.Empty;
         EstaturaEntry.Text = string.Empty;
+        SitucionNutricionalLabel.Text = string.Empty;
         ImcLabel.Text = string.Empty;
-        SituacionNutricionalLabel.Text = string.Empty;
+
+
     }
 }
